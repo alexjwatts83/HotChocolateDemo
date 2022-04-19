87c51ad2501ff2f153a2ebf82e51abf8bcf797be
Alex Watts <alexjwatts83@gmail.com>
2022-04-20 07:04:06 +1000

    WIP EF and models

---
+++ CommanderGQLApi/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CommanderGQLApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommanderGQLApi.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Platform> Platforms { get; set; }
+    }
+}
