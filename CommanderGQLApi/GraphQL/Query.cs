a07d4974b43e08451ffe40acb0699d3e13eb4450
Alex Watts <alexjwatts83@gmail.com>
2022-04-22 07:47:49 +1000

    Added Query

---
+++ CommanderGQLApi/GraphQL/Query.cs
@@ -0,0 +1,15 @@
+﻿using CommanderGQLApi.Data;
+using CommanderGQLApi.Models;
+using HotChocolate;
+using System.Linq;
+
+namespace CommanderGQLApi.GraphQL
+{
+    public class Query
+    {
+        public IQueryable<Platform> GetPlatform([Service] AppDbContext context)
+        {
+            return context.Platforms;
+        }
+    }
+}
