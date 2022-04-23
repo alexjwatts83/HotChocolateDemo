d1313ff455a698c7d91037e1990e45f6dd9aac8d
Alex Watts <alexjwatts83@gmail.com>
2022-04-23 18:36:08 +1000

    removued unused usings

--- CommanderGQLApi/Data/AppDbContext.cs
+++ CommanderGQLApi/Data/AppDbContext.cs
@@ -1,9 +1,5 @@
 ﻿using CommanderGQLApi.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CommanderGQLApi.Data
 {
