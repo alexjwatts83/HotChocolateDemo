c8e60b325ee634a3dacf6f5511740e7566487776
Alex Watts <alexjwatts83@gmail.com>
2022-04-24 20:58:07 +1000

    Added EF projections

--- CommanderGQLApi/GraphQL/Query.cs
+++ CommanderGQLApi/GraphQL/Query.cs
@@ -9,6 +9,7 @@ namespace CommanderGQLApi.GraphQL
     public class Query
     {
         [UseDbContext(typeof(AppDbContext))]
+        [UseProjection]
         public IQueryable<Platform> GetPlatform([ScopedService] AppDbContext context)
         {
             return context.Platforms;
--- CommanderGQLApi/Startup.cs
+++ CommanderGQLApi/Startup.cs
@@ -26,7 +26,8 @@ namespace CommanderGQLApi
 
             services
                 .AddGraphQLServer()
-                .AddQueryType<Query>();
+                .AddQueryType<Query>()
+                .AddProjections();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
