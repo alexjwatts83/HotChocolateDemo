4c2f7e281df141d504b67f6038233ebe29b0ef06
Alex Watts <alexjwatts83@gmail.com>
2022-04-24 20:47:59 +1000

    Updated db context

--- CommanderGQLApi/Data/AppDbContext.cs
+++ CommanderGQLApi/Data/AppDbContext.cs
@@ -11,5 +11,23 @@ namespace CommanderGQLApi.Data
         }
 
         public DbSet<Platform> Platforms { get; set; }
+        public DbSet<Command> Commands { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Platform has many commands
+            modelBuilder
+                .Entity<Platform>()
+                .HasMany(p => p.Commands)
+                .WithOne(p => p.Platform!)
+                .HasForeignKey(p => p.PlatformId);
+
+            // Commands has one Platform
+            modelBuilder
+                .Entity<Command>()
+                .HasOne(p => p.Platform)
+                .WithMany(p => p.Commands)
+                .HasForeignKey(p => p.PlatformId);
+        }
     }
 }
