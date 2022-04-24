ca61c3980af17a55da4262d51799771ca79358a3
Alex Watts <alexjwatts83@gmail.com>
2022-04-24 21:13:33 +1000

    Updated Query to include Commands

--- CommanderGQLApi/GraphQL/Query.cs
+++ CommanderGQLApi/GraphQL/Query.cs
@@ -6,13 +6,36 @@ using System.Linq;
 
 namespace CommanderGQLApi.GraphQL
 {
+    /// <summary>
+    /// Represents the queries available.
+    /// </summary>
+    [GraphQLDescription("Represents the queries available.")]
     public class Query
     {
+        /// <summary>
+        /// Gets the queryable <see cref="Platform"/>.
+        /// </summary>
+        /// <param name="context">The <see cref="AppDbContext"/>.</param>
+        /// <returns>The queryable <see cref="Platform"/>.</returns>
         [UseDbContext(typeof(AppDbContext))]
         [UseProjection]
+        [GraphQLDescription("Gets the queryable platform.")]
         public IQueryable<Platform> GetPlatform([ScopedService] AppDbContext context)
         {
             return context.Platforms;
+        }
+
+        /// <summary>
+        /// Gets the queryable <see cref="Command"/>.
+        /// </summary>
+        /// <param name="context">The <see cref="AppDbContext"/>.</param>
+        /// <returns>The queryable <see cref="Command"/>.</returns>
+        [UseDbContext(typeof(AppDbContext))]
+        [UseProjection]
+        [GraphQLDescription("Gets the queryable command.")]
+        public IQueryable<Command> GetCommand([ScopedService] AppDbContext context)
+        {
+            return context.Commands;
         }
     }
 }
