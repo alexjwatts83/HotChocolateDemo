e17157a1b3bac7c9e7c371ae6c60e1f573ad899b
Alex Watts <alexjwatts83@gmail.com>
2022-04-25 08:25:47 +1000

    Added mutation to add a platform but no commands as of yet

---
+++ CommanderGQLApi/GraphQL/Mutation.cs
@@ -0,0 +1,29 @@
+﻿using CommanderGQLApi.Data;
+using CommanderGQLApi.GraphQL.Platforms;
+using CommanderGQLApi.Models;
+using HotChocolate;
+using HotChocolate.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommanderGQLApi.GraphQL
+{
+    [GraphQLDescription("Represents the mutations available.")]
+    public class Mutation
+    {
+        [UseDbContext(typeof(AppDbContext))]
+        public async Task<AddPlatformPayload> AddPlatformAsync(AddPlatformInput input, [ScopedService] AppDbContext context)
+        {
+            var platform = new Platform
+            {
+                Name = input.Name
+            };
+
+            context.Platforms.Add(platform);
+            await context.SaveChangesAsync();
+
+            return new AddPlatformPayload(platform)
+;
+        }
+    }
+}
